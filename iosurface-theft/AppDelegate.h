2d0dd829760134b7a43f00c8997912abc0a3756d
Jevin Sweval <jevinsweval@gmail.com>
2021-02-26 12:35:33 -0800

    Initial Commit

---
+++ iosurface-theft/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iosurface-theft
+//
+//  Created by Jevin Sweval on 2/26/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ iosurface-theft/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iosurface-theft
+//
+//  Created by Jevin Sweval on 2/26/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
